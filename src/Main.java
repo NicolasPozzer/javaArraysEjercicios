2523be04ca7165e8926870c2ddfc128a23d7568a
NicolasPozzer <pozzernico@gmail.com>
2023-07-15 20:49:28 -0300

    update-ejer4-finalizado

--- src/Main.java
+++ src/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
 
 
-        //---EJERCICIO 1 ARRAYS---
+        /*//---EJERCICIO 2 ARRAYS---
 
         //objetivo: recorrer la matriz mientras que en la 4ta columna
         //se muestra las 3 notas del alumno y el promedio de dicho alumno.
@@ -60,11 +60,63 @@ public class Main {
             }
 
 
+        }*/
+
+
+
+
+        //---EJERCICIO 4 ARRAYS---
+        // 3 vectores
+        //nombre - temperatura minima - temp maxima
+
+        //variables necesarias
+
+        String nombreTempMin;
+        String nombreTempMax;
+        double mayor;
+        double menor;
+
+        mayor = 0.00;
+        menor = 999999.99;
+        nombreTempMin = "";
+        nombreTempMax = "";
+
+        //creamos los 3 vectores
+        String[] ciudades = new String[5];
+        double[] tempMin = new double[5];
+        double[] tempMax = new double[5];
+
+
+        //cargamos los vectores
+        Scanner teclado = new Scanner(System.in);
+        Scanner teclado2 = new Scanner(System.in);
+        Scanner teclado3 = new Scanner(System.in);
+
+        for (int i=0; i < ciudades.length; i++){
+            System.out.println("Ingrese el nombre de la ciudad " + i + ": ");
+            ciudades[i] = teclado.next();
+
+            System.out.println("Ingrese la temperatura minima de la ciudad " + i + ": ");
+            tempMin[i] = teclado2.nextInt();
+
+            System.out.println("Ingrese la temperatura maxima de la ciudad " + i + ": ");
+            tempMax[i] = teclado3.nextInt();
+
+            if (tempMin[i] < menor){
+                menor = tempMin[i];
+                nombreTempMin = ciudades[i];
+            }
+            if (tempMax[i] > mayor){
+                mayor = tempMax[i];
+                nombreTempMax = ciudades[i];
+            }
+
         }
 
-
-
-
+        System.out.println("\nLa ciudad con menor temperatura es: "+ nombreTempMin+ " con una " +
+                "temperatura de: "+ menor);
+        System.out.println("La ciudad con mayor temperatura es: "+ nombreTempMax+ " con una " +
+                "temperatura de: "+ mayor);
 
 
     }
